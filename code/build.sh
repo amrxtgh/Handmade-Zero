566d04ec16a35e7b182cd5ffc897a55ee646e00c
amrxtgh <ghimireamrit619@gmail.com>
2026-07-14 06:01:50 +0545

    Update: build.sh

--- code/build.sh
+++ code/build.sh
@@ -8,7 +8,8 @@ cd ../build
 # -Wall: Warnings all
 # -Wextra: Enables even more warnings that are useful but might trigger on the valid code patterns(unused function parameters, override mismatches etc)
 # -Werror: To enables all the warnings as the compilation error.
-clang -g -O0 -Wall -Wextra ../code/sdl_handmade.cpp -o handmade_hero $(pkg-config --cflags --libs sdl2) -lm
+clang -g -O0 -Wall -Wextra ../code/sdl_handmade.cpp -o handmade_hero $(pkg-config --cflags --libs sdl3) -lm
 echo "Build successful -> build/handmade_hero"
+../build/handmade_hero
 
 cd ../code
